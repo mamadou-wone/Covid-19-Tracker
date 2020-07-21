bce4f174db13fad1f2949d78142288fa2204ef3f
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-21 17:50:05 +0000

    Days23 #100DaysOfCode [23/100] Start Covid Tracker

---
+++ covid.js
@@ -0,0 +1,83 @@
+let select = document.getElementById('select');
+let countryTable = document.querySelector('tbody');
+let totalCase = document.getElementById('totalCases');
+let recovered = document.getElementById('recovered');
+let deaths = document.getElementById('deaths');
+
+
+
+
+const apiUrl = 'https://disease.sh/v3/covid-19/countries/';
+const allCaseApi = 'https://disease.sh/v3/covid-19/all/';
+
+
+function getAllCase() {
+    let request = new XMLHttpRequest();
+    request.open('GET', allCaseApi);
+    request.responseType = 'json';
+    request.send();
+
+
+    request.onload = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                let response = request.response;
+                document.querySelector('.totalCases').textContent = response.cases;
+                document.querySelector('.recovered').textContent = response.recovered;
+                document.querySelector('.deaths').textContent = response.deaths;
+                document.querySelector('.plusCases').textContent = response.todayCases;
+                document.querySelector('.plusRecovered').textContent = response.todayRecovered;
+                document.querySelector('.plusDeath').textContent = response.todayDeaths;
+            } else {
+                alert('Une erreur est intervenue');
+            }
+        }
+    }
+}
+
+getAllCase();
+
+function getCountryCase() {
+    let request = new XMLHttpRequest();
+    request.open('GET', apiUrl);
+    request.responseType = 'json';
+    request.send();
+
+
+    request.onload = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                let response = request.response;
+                for (let index = 0; index < response.length; index++) {
+                    const element = response[index];
+                    let option = document.createElement('option');
+                    option.textContent = element.country;
+                    select.append(option);
+
+                    let trCountry = document.createElement('tr');
+                    let tdCountry = document.createElement('td');
+                    let tdCases = document.createElement('td');
+                    tdCountry.textContent = element.country;
+                    tdCases.textContent = element.cases;
+                    trCountry.append(tdCountry);
+                    trCountry.append(tdCases);
+                    countryTable.append(trCountry);
+
+                    trCountry.addEventListener('click', () => {
+                        document.querySelector('.totalCases').textContent = element.cases;
+                        document.querySelector('.recovered').textContent = element.recovered;
+                        document.querySelector('.deaths').textContent = element.deaths;
+                        document.querySelector('.plusCases').textContent = element.todayCases;
+                        document.querySelector('.plusRecovered').textContent = element.todayRecovered;
+                        document.querySelector('.plusDeath').textContent = element.todayDeaths;
+                    })
+
+                }
+            } else {
+                alert('Une erreur est intervenue');
+            }
+        }
+    }
+}
+
+getCountryCase();
