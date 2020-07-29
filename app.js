541278c7c35e01886d4127b95cf80d91b9893c96
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-29 16:15:37 +0000

    Day31 #100DaysOfCode [31/100]

---
+++ app.js
@@ -0,0 +1,44 @@
+const allCountries = 'https://disease.sh/v3/covid-19/countries/';
+const word = 'https://disease.sh/v3/covid-19/all/';
+
+function getWordStat() {
+    let request = new XMLHttpRequest();
+    request.open('GET', word);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                let response = request.response;
+                console.log(response);
+            } else {
+                alert('Une erreur est survenue !');
+            }
+        }
+    }
+}
+
+
+function getCaseByCountry() {
+    let request = new XMLHttpRequest();
+    request.open('GET', allCountries);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                let response = request.response;
+                console.log(response);
+            } else {
+                alert('Une erreur est survenue !');
+            }
+        }
+    }
+}
+
+
+
+
+// getWordStat();
