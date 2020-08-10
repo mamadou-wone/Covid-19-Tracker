b2fd52d7d5da722b9eed9721b19b047d3b08ac63
Mamadou WONE <mamadouwone12345@gmail.com>
2020-08-10 18:52:14 +0000

    Day42 #100DaysOfCode [42/100]

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const allCountries = 'https://disease.sh/v3/covid-19/countries/';
 const word = 'https://disease.sh/v3/covid-19/all/';
-let caseTab = [
+let countrieInfo = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
     [],
     [],
     []
 ];
 
-let newCaseTab = [
+let newCountrieInfo = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
     [],
     [],
     []
@@ -56,13 +70,27 @@ function getCaseByCountry() {
                 let response = request.response;
                 for (let index = 0; index < response.length; index++) {
                     const element = response[index];
-                    newCaseTab[0].push(element.cases);
-                    newCaseTab[1].push(element.country);
-                    newCaseTab[2].push(element.countryInfo.flag);
-                    caseTab[0].push(element.cases);
-                    caseTab[1].push(element.country);
-                    caseTab[2].push(element.countryInfo.flag);
-
+                    newCountrieInfo[0].push(element.cases);
+                    newCountrieInfo[1].push(element.country);
+                    newCountrieInfo[2].push(element.countryInfo.flag);
+                    newCountrieInfo[3].push(element.todayCases);
+                    newCountrieInfo[4].push(element.deaths);
+                    newCountrieInfo[5].push(element.todayDeaths);
+                    newCountrieInfo[6].push(element.recovered);
+                    newCountrieInfo[7].push(element.todayRecovered);
+                    newCountrieInfo[8].push(element.countryInfo.lat); //26.countryInfo.lat
+                    newCountrieInfo[9].push(element.countryInfo.long);
+                    // Tableau Initial
+                    countrieInfo[0].push(element.cases);
+                    countrieInfo[1].push(element.country);
+                    countrieInfo[2].push(element.countryInfo.flag);
+                    countrieInfo[3].push(element.todayCases);
+                    countrieInfo[4].push(element.deaths);
+                    countrieInfo[5].push(element.todayDeaths);
+                    countrieInfo[6].push(element.recovered);
+                    countrieInfo[7].push(element.todayRecovered);
+                    countrieInfo[8].push(element.countryInfo.lat);
+                    countrieInfo[9].push(element.countryInfo.long);
 
                     let option = document.createElement('option');
                     option.style.backgroundImage = `url(${element.countryInfo.flag})`;
@@ -78,17 +106,26 @@ function getCaseByCountry() {
         }
 
 
-        caseTab[0].sort(function(a, b) {
+        countrieInfo[0].sort(function(a, b) {
             return b - a;
         });
-        // console.log(caseTab[0][0], caseTab[1][newCaseTab[0].indexOf(caseTab[0][0])]);
-        // console.log(caseTab);
-        // console.log(newCaseTab[0].indexOf(caseTab[0][0]));
+        console.log(countrieInfo);
+        // console.log(countrieInfo[0][0], countrieInfo[1][newCountrieInfo[0].indexOf(countrieInfo[0][0])]);
+        // console.log(countrieInfo);
+        // console.log(newCountrieInfo[0].indexOf(countrieInfo[0][0]));
 
-        for (let index = 0; index < caseTab[0].length; index++) {
-            const element = caseTab[0][index];
-            const indexCountry = caseTab[1][newCaseTab[0].indexOf(caseTab[0][index])];
-            const indexFlag = caseTab[2][newCaseTab[0].indexOf(caseTab[0][index])];
+        for (let index = 0; index < countrieInfo[0].length; index++) {
+            const element = countrieInfo[0][index];
+            const indexCountry = countrieInfo[1][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexFlag = countrieInfo[2][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexTodayCases = countrieInfo[3][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexDeath = countrieInfo[4][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexTodayDeaths = countrieInfo[5][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexRecovered = countrieInfo[6][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexTodayRecovered = countrieInfo[7][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexLat = countrieInfo[8][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+            const indexLong = countrieInfo[9][newCountrieInfo[0].indexOf(countrieInfo[0][index])];
+
             let tr = document.createElement('tr');
             let tdFlag = document.createElement('td');
             let flag = document.createElement('img');
@@ -99,11 +136,17 @@ function getCaseByCountry() {
             let tdCountrie = document.createElement('td');
             tdCountrie.textContent = indexCountry;
             let tdCase = document.createElement('td');
-            tdCase.textContent = new Intl.NumberFormat('ja-JP').format(caseTab[0][index]);
+            tdCase.textContent = new Intl.NumberFormat('ja-JP').format(countrieInfo[0][index]);
             tr.append(tdFlag, tdCountrie, tdCase);
             document.querySelector('tbody').append(tr);
             tr.addEventListener('click', () => {
-                console.log('click');
+                console.log(indexLat);
+                document.querySelector('#totalCase').textContent = suroundNumber(countrieInfo[0][index]) + ' Total';
+                document.querySelector("#todayNew").textContent = new Intl.NumberFormat('ja-JP').format(indexTodayCases) + ' Today';
+                document.querySelector('#todayRecovered').textContent = new Intl.NumberFormat('ja-JP').format(indexTodayRecovered) + ' Today';
+                document.querySelector('#totalRecovered').textContent = suroundNumber(indexRecovered) + ' Total';
+                document.querySelector('#todayDeath').textContent = new Intl.NumberFormat('ja-JP').format(indexTodayDeaths) + ' Today';
+                document.querySelector('#totalDeath').textContent = suroundNumber(indexDeath) + ' Total';
             })
         }
 
